a896a72406e809d89682f4f5fe1fdca9813f9a87
taoqf <tao_qf@163.com>
2020-03-08 14:20:45 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
+import * as dot from 'dot';
+import { HTMLElement } from 'node-html-parser';
 
-export default function an6(param1: string, param2: string, param3: string, param4: string) {
+/**
+ * html渲染
+ *
+ * @param dom_node 结点对象，p应位于该结点之内
+ * @param data 数据
+ * @param tpl dot模板字符串
+ * @param position_id p名称，在html上属性`data-feidao-presentation`对应的属性值
+ */
+export default function render(dom_node: HTMLElement, data: unknown, tpl: string, position_id: string) {
+	const res = dot.template(tpl)(data);
+	const list = dom_node.querySelectorAll(`[data-mm-tpl=${position_id}]`);
+	if (!list || list.length === 0) {
+		throw new Error(`cannot find node: ${position_id}`);
+	} else {
+		list.forEach((n) => {
+			n.set_content(res || '');
+		});
+	}
 }
--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,11 @@
 const test = require('ava');
+const { default: parse } = require('node-html-parser');
 
 const { default: a } = require('../dist/index');
 
-test('xxx', (t) => {
-	t.pass();
+test('render', async (t) => {
+	const root = await parse('<div><div data-mm-tpl="p01"></div></div>');
+	const div = root.firstChild;
+	await a(div, {}, '<foo>bar</foo>', 'p01');
+	t.is(div.toString(), '<div><div data-mm-tpl="p01"><foo>bar</foo></div></div>');
 });
